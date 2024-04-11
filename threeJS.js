465f0b76de873a4a392d23617d3a82257d3d67ad
0NDRS <ondrazab2006@gmail.com>
2024-04-11 10:46:54 +0200

    feat: gitignore & package.json scripts

--- threeJS.js
+++ threeJS.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
