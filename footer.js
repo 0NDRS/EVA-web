ceb7068f93d8e240371da2fd214cf838516940bc
Ondřej Zábranský <145441873+0NDRS@users.noreply.github.com>
2024-05-12 12:11:32 +0200

    Delete footer.js

--- footer.js
+++
@@ -1,6 +0,0 @@
-console.log("footer.js is connected");
-
-document.addEventListener("DOMContentLoaded", function() {
-    const currentYear = new Date().getFullYear();
-    document.getElementById("year").innerHTML = currentYear;
-});
